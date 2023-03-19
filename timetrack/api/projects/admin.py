933cc4eb6269603ba428d1872860ee4ea0ef6d1a
ahmadjanan <ahmed.ali1@arbisoft.com>
2023-03-19 16:06:53 +0500

    added django admin support

--- timetrack/api/projects/admin.py
+++ timetrack/api/projects/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from api.projects.models import Project, ProjectMember, TimeLog
+
+admin.site.register(Project)
+admin.site.register(ProjectMember)
+admin.site.register(TimeLog)
--- timetrack/api/users/admin.py
+++ timetrack/api/users/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+
+from api.users.models import TimeTrackUser
+
+admin.site.register(TimeTrackUser)
