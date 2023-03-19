9821be376cd94fc839e420955d449906ab32e874
ahmadjanan <ahmed.ali1@arbisoft.com>
2023-03-19 23:03:25 +0500

    refactored project permissions tests

--- timetrack/api/projects/tests/test_project_permissions.py
+++ timetrack/api/projects/tests/test_project_permissions.py
@@ -4,56 +4,46 @@ from api.projects.tests.factories import ProjectFactory
 from api.users.tests.factories import TimeTrackUserFactory
 
 
+@pytest.fixture
+def project():
+    user = TimeTrackUserFactory.with_password(password="secret123")
+    project = ProjectFactory.with_owner(owner=user)
+
+    return project
+
+
 @pytest.mark.django_db
-def test_project_safe_methods(client):
+def test_project_owner_safe_method(client, project):
     """
-    Test project safe method
+    Test project owner safe method
     """
-    first_user = TimeTrackUserFactory.with_password(password="secret123")
-    second_user = TimeTrackUserFactory.with_password(
-        email="user2@test.com",
-        username="user2",
-        password="secret123",
-    )
-    project = ProjectFactory.with_owner(owner=first_user)
-
-    client.force_authenticate(user=second_user)
-    resp = client.get(f"/api/projects/{project.uuid}")
-    assert resp.status_code == 200
-
-    client.force_authenticate(user=first_user)
+    client.force_authenticate(user=project.owner)
     resp = client.get(f"/api/projects/{project.uuid}")
     assert resp.status_code == 200
 
 
 @pytest.mark.django_db
-def test_project_unsafe_methods(client):
+def test_project_non_owner_safe_method(client, project):
     """
-    Test project unsafe methods
+    Test project non-owner safe method
     """
-    first_user = TimeTrackUserFactory.with_password(password="secret123")
-    second_user = TimeTrackUserFactory.with_password(
+    user = TimeTrackUserFactory.with_password(
         email="user2@test.com",
         username="user2",
         password="secret123",
     )
-    project = ProjectFactory.with_owner(owner=first_user)
 
-    client.force_authenticate(user=second_user)
-    resp = client.delete(f"/api/projects/{project.uuid}")
-    assert resp.status_code == 403
-    resp = client.patch(f"/api/projects/{project.uuid}")
-    assert resp.status_code == 403
-    resp = client.put(
-        f"/api/projects/{project.uuid}",
-        data={
-            "name": "Updated project name",
-            "description": "Updated project description",
-        }
-    )
-    assert resp.status_code == 403
+    client.force_authenticate(user=user)
+    resp = client.get(f"/api/projects/{project.uuid}")
+    assert resp.status_code == 200
 
-    client.force_authenticate(user=first_user)
+
+@pytest.mark.django_db
+def test_project_owner_unsafe_methods(client, project):
+    """
+    Test project owner unsafe methods
+    """
+    client.force_authenticate(user=project.owner)
     resp = client.patch(f"/api/projects/{project.uuid}")
     assert resp.status_code == 200
     resp = client.put(
@@ -66,3 +56,29 @@ def test_project_unsafe_methods(client):
     assert resp.status_code == 200
     resp = client.delete(f"/api/projects/{project.uuid}")
     assert resp.status_code == 204
+
+
+@pytest.mark.django_db
+def test_project_non_owner_unsafe_methods(client, project):
+    """
+    Test project non-owner unsafe methods
+    """
+    user = TimeTrackUserFactory.with_password(
+        email="user2@test.com",
+        username="user2",
+        password="secret123",
+    )
+
+    client.force_authenticate(user=user)
+    resp = client.delete(f"/api/projects/{project.uuid}")
+    assert resp.status_code == 403
+    resp = client.patch(f"/api/projects/{project.uuid}")
+    assert resp.status_code == 403
+    resp = client.put(
+        f"/api/projects/{project.uuid}",
+        data={
+            "name": "Updated project name",
+            "description": "Updated project description",
+        }
+    )
+    assert resp.status_code == 403
