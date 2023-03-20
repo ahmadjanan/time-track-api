c84d13cea45ccabb03c9cd6b5432f1d71a7f1f58
ahmadjanan <ahmed.ali1@arbisoft.com>
2023-03-20 13:27:06 +0500

    removed token obtain custom view

--- timetrack/api/users/urls.py
+++ timetrack/api/users/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView, TokenObtainPairView
 
 from api.users.views.change_password import ChangePasswordView
 from api.users.views.create_view import TimeTrackUserCreateView
 from api.users.views.retrieve_update_view import TimeTrackUserRetrieveUpdateView
-from api.users.views.token_obtain_view import MyTokenObtainPairView
 
 urlpatterns = [
     path('register', TimeTrackUserCreateView.as_view(), name='create-user'),
     path('profile', TimeTrackUserRetrieveUpdateView.as_view(), name='retrieve-update-profile'),
-    path('login', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('login', TokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token-refresh'),
     path('token/verify', TokenVerifyView.as_view(), name='token-verify'),
     path('change_password', ChangePasswordView.as_view(), name='password-change'),
--- timetrack/api/users/views/token_obtain_view.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-USER_MODEL = get_user_model()
-
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = TokenObtainPairSerializer
