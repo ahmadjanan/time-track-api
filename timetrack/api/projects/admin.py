731d319547079b853b890b80ea3cbf2c90313b8f
ahmadjanan <ahmed.ali1@arbisoft.com>
2023-03-21 14:07:55 +0500

    customized Django model admins

--- timetrack/api/projects/admin.py
+++ timetrack/api/projects/admin.py
@@ -2,6 +2,62 @@ from django.contrib import admin
 
 from api.projects.models import Project, ProjectMember, TimeLog
 
-admin.site.register(Project)
-admin.site.register(ProjectMember)
-admin.site.register(TimeLog)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """
+    Project Model Django Admin configuration
+    """
+    list_display = ('uuid', 'name', 'owner', 'description')
+    search_fields = ('uuid', 'name', 'owner__username', 'description')
+    list_filter = ('uuid', 'owner',)
+    readonly_fields = ('uuid', )
+
+    fieldsets = (
+        ('Details', {
+            'fields': ('name', 'description', 'uuid', )
+        }),
+        ('Ownership', {
+            'fields': ('owner',)
+        })
+    )
+
+
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    """
+    ProjectMember Model Django Admin configuration
+    """
+    list_display = ('uuid', 'project', 'user', 'status', )
+    search_fields = ('uuid', 'project__name', 'user__username', 'status', )
+    list_filter = ('uuid', 'user', 'project', )
+    readonly_fields = ('uuid', )
+
+    fieldsets = (
+        ('Details', {
+            'fields': ('uuid', )
+        }),
+        ('Membership', {
+            'fields': ('project', 'user', 'status', )
+        })
+    )
+
+
+@admin.register(TimeLog)
+class TimeLogAdmin(admin.ModelAdmin):
+    """
+    TimeLog Model Django Admin configuration
+    """
+    list_display = ('uuid', 'member', 'start_time', 'end_time', )
+    search_fields = ('uuid', 'member', )
+    list_filter = ('uuid', )
+    readonly_fields = ('uuid', )
+
+    fieldsets = (
+        ('Details', {
+            'fields': ('uuid', 'description', 'member', )
+        }),
+        ('Log', {
+            'fields': ('start_time', 'end_time', )
+        })
+    )
--- timetrack/api/users/admin.py
+++ timetrack/api/users/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from api.users.models import TimeTrackUser
 
-admin.site.register(TimeTrackUser)
+
+@admin.register(TimeTrackUser)
+class TimeTrackUserAdmin(UserAdmin):
+    """
+    TimeTrackUser Custom User model Admin
+    """
+    list_display = ('email', 'get_full_name', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
